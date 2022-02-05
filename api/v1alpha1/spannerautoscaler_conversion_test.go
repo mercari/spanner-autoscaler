76360646f81dc729523dae8af95fa4c1b882da87
shuheiktgw <s-kitagawa@mercari.com>
2022-02-05 10:58:52 +0900

    Use Ginkgo instead

--- api/v1alpha1/spannerautoscaler_conversion_test.go
+++ api/v1alpha1/spannerautoscaler_conversion_test.go
@@ -2,330 +2,189 @@ package v1alpha1
 
 import (
 	"github.com/mercari/spanner-autoscaler/api/v1beta1"
-	"github.com/mercari/spanner-autoscaler/utils/pointer"
-	"github.com/stretchr/testify/require"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
+	"k8s.io/utils/pointer"
 	"time"
 )
 
-func TestSpannerAutoscaler_ConvertTo(t *testing.T) {
-	t.Parallel()
-	timestamp := time.Now()
+var _ = Describe("ConvertTo", func() {
+	var dest *v1beta1.SpannerAutoscaler
+	var src *SpannerAutoscaler
+	var expected *v1beta1.SpannerAutoscaler
+	var timestamp time.Time
 
-	tests := []struct {
-		title    string
-		src      *SpannerAutoscaler
-		expected *v1beta1.SpannerAutoscaler
-	}{
-		{
-			title: "target instance",
-			src: &SpannerAutoscaler{
-				Spec: SpannerAutoscalerSpec{
-					ScaleTargetRef: ScaleTargetRef{
-						ProjectID:  pointer.String("src-project-id"),
-						InstanceID: pointer.String("src-instance-id"),
-					},
-					TargetCPUUtilization: TargetCPUUtilization{
-						HighPriority: pointer.Int32(50),
+	BeforeEach(func() {
+		dest = &v1beta1.SpannerAutoscaler{}
+		src = &SpannerAutoscaler{
+			Spec: SpannerAutoscalerSpec{
+				ScaleTargetRef: ScaleTargetRef{
+					ProjectID:  pointer.String("src-project-id"),
+					InstanceID: pointer.String("src-instance-id"),
+				},
+				TargetCPUUtilization: TargetCPUUtilization{
+					HighPriority: pointer.Int32(50),
+				},
+			},
+		}
+		expected = &v1beta1.SpannerAutoscaler{
+			Spec: v1beta1.SpannerAutoscalerSpec{
+				TargetInstance: v1beta1.TargetInstance{
+					ProjectID:  "src-project-id",
+					InstanceID: "src-instance-id",
+				},
+				ScaleConfig: v1beta1.ScaleConfig{
+					TargetCPUUtilization: v1beta1.TargetCPUUtilization{
+						HighPriority: 50,
 					},
 				},
 			},
-			expected: &v1beta1.SpannerAutoscaler{
-				Spec: v1beta1.SpannerAutoscalerSpec{
-					TargetInstance: v1beta1.TargetInstance{
-						ProjectID:  "src-project-id",
-						InstanceID: "src-instance-id",
-					},
-					ScaleConfig: v1beta1.ScaleConfig{
-						TargetCPUUtilization: v1beta1.TargetCPUUtilization{
-							HighPriority: 50,
-						},
-					},
-				},
-			},
-		},
-		{
-			title: "authentication AuthTypeImpersonation",
-			src: &SpannerAutoscaler{
-				Spec: SpannerAutoscalerSpec{
-					ScaleTargetRef: ScaleTargetRef{
-						ProjectID:  pointer.String("src-project-id"),
-						InstanceID: pointer.String("src-instance-id"),
-					},
-					ImpersonateConfig: &ImpersonateConfig{
-						TargetServiceAccount: "src-target-service-account",
-						Delegates:            []string{"src-delegate1", "src-delegate2"},
-					},
-					TargetCPUUtilization: TargetCPUUtilization{
-						HighPriority: pointer.Int32(50),
-					},
-				},
-			},
-			expected: &v1beta1.SpannerAutoscaler{
-				Spec: v1beta1.SpannerAutoscalerSpec{
-					TargetInstance: v1beta1.TargetInstance{
-						ProjectID:  "src-project-id",
-						InstanceID: "src-instance-id",
-					},
-					Authentication: v1beta1.Authentication{
-						Type: v1beta1.AuthTypeImpersonation,
-						ImpersonateConfig: &v1beta1.ImpersonateConfig{
-							TargetServiceAccount: "src-target-service-account",
-							Delegates:            []string{"src-delegate1", "src-delegate2"},
-						},
-					},
-					ScaleConfig: v1beta1.ScaleConfig{
-						TargetCPUUtilization: v1beta1.TargetCPUUtilization{
-							HighPriority: 50,
-						},
-					},
-				},
-			},
-		},
-		{
-			title: "authentication AuthTypeSA with Namespace",
-			src: &SpannerAutoscaler{
-				Spec: SpannerAutoscalerSpec{
-					ScaleTargetRef: ScaleTargetRef{
-						ProjectID:  pointer.String("src-project-id"),
-						InstanceID: pointer.String("src-instance-id"),
-					},
-					ServiceAccountSecretRef: &ServiceAccountSecretRef{
-						Name:      pointer.String("src-service-account-secret-name"),
-						Namespace: pointer.String("src-service-account-secret-namespace"),
-						Key:       pointer.String("src-service-account-secret-key"),
-					},
-					TargetCPUUtilization: TargetCPUUtilization{
-						HighPriority: pointer.Int32(50),
-					},
-				},
-			},
-			expected: &v1beta1.SpannerAutoscaler{
-				Spec: v1beta1.SpannerAutoscalerSpec{
-					TargetInstance: v1beta1.TargetInstance{
-						ProjectID:  "src-project-id",
-						InstanceID: "src-instance-id",
-					},
-					Authentication: v1beta1.Authentication{
-						Type: v1beta1.AuthTypeSA,
-						IAMKeySecret: &v1beta1.IAMKeySecret{
-							Name:      "src-service-account-secret-name",
-							Namespace: "src-service-account-secret-namespace",
-							Key:       "src-service-account-secret-key",
-						},
-					},
-					ScaleConfig: v1beta1.ScaleConfig{
-						TargetCPUUtilization: v1beta1.TargetCPUUtilization{
-							HighPriority: 50,
-						},
-					},
-				},
-			},
-		},
-		{
-			title: "authentication AuthTypeSA without Namespace",
-			src: &SpannerAutoscaler{
-				Spec: SpannerAutoscalerSpec{
-					ScaleTargetRef: ScaleTargetRef{
-						ProjectID:  pointer.String("src-project-id"),
-						InstanceID: pointer.String("src-instance-id"),
-					},
-					ServiceAccountSecretRef: &ServiceAccountSecretRef{
-						Name: pointer.String("src-service-account-secret-name"),
-						Key:  pointer.String("src-service-account-secret-key"),
-					},
-					TargetCPUUtilization: TargetCPUUtilization{
-						HighPriority: pointer.Int32(50),
-					},
-				},
-			},
-			expected: &v1beta1.SpannerAutoscaler{
-				Spec: v1beta1.SpannerAutoscalerSpec{
-					TargetInstance: v1beta1.TargetInstance{
-						ProjectID:  "src-project-id",
-						InstanceID: "src-instance-id",
-					},
-					Authentication: v1beta1.Authentication{
-						Type: v1beta1.AuthTypeSA,
-						IAMKeySecret: &v1beta1.IAMKeySecret{
-							Name: "src-service-account-secret-name",
-							Key:  "src-service-account-secret-key",
-						},
-					},
-					ScaleConfig: v1beta1.ScaleConfig{
-						TargetCPUUtilization: v1beta1.TargetCPUUtilization{
-							HighPriority: 50,
-						},
-					},
-				},
-			},
-		},
-		{
-			title: "scaleConfig ComputeTypeNode",
-			src: &SpannerAutoscaler{
-				Spec: SpannerAutoscalerSpec{
-					ScaleTargetRef: ScaleTargetRef{
-						ProjectID:  pointer.String("src-project-id"),
-						InstanceID: pointer.String("src-instance-id"),
-					},
-					MinNodes:          pointer.Int32(1),
-					MaxNodes:          pointer.Int32(10),
-					MaxScaleDownNodes: pointer.Int32(1),
-					TargetCPUUtilization: TargetCPUUtilization{
-						HighPriority: pointer.Int32(50),
-					},
-				},
-			},
-			expected: &v1beta1.SpannerAutoscaler{
-				Spec: v1beta1.SpannerAutoscalerSpec{
-					TargetInstance: v1beta1.TargetInstance{
-						ProjectID:  "src-project-id",
-						InstanceID: "src-instance-id",
-					},
-					ScaleConfig: v1beta1.ScaleConfig{
-						ComputeType: v1beta1.ComputeTypeNode,
-						Nodes: v1beta1.ScaleConfigNodes{
-							Min: 1,
-							Max: 10,
-						},
-						ScaledownStepSize: 1,
-						TargetCPUUtilization: v1beta1.TargetCPUUtilization{
-							HighPriority: 50,
-						},
-					},
-				},
-			},
-		},
-		{
-			title: "scaleConfig ComputeTypePU",
-			src: &SpannerAutoscaler{
-				Spec: SpannerAutoscalerSpec{
-					ScaleTargetRef: ScaleTargetRef{
-						ProjectID:  pointer.String("src-project-id"),
-						InstanceID: pointer.String("src-instance-id"),
-					},
-					MinProcessingUnits: pointer.Int32(100),
-					MaxProcessingUnits: pointer.Int32(1000),
-					MaxScaleDownNodes:  pointer.Int32(1),
-					TargetCPUUtilization: TargetCPUUtilization{
-						HighPriority: pointer.Int32(50),
-					},
-				},
-			},
-			expected: &v1beta1.SpannerAutoscaler{
-				Spec: v1beta1.SpannerAutoscalerSpec{
-					TargetInstance: v1beta1.TargetInstance{
-						ProjectID:  "src-project-id",
-						InstanceID: "src-instance-id",
-					},
-					ScaleConfig: v1beta1.ScaleConfig{
-						ComputeType: v1beta1.ComputeTypePU,
-						ProcessingUnits: v1beta1.ScaleConfigPUs{
-							Min: 100,
-							Max: 1000,
-						},
-						ScaledownStepSize: 1,
-						TargetCPUUtilization: v1beta1.TargetCPUUtilization{
-							HighPriority: 50,
-						},
-					},
-				},
-			},
-		},
-		{
-			title: "objectMeta",
-			src: &SpannerAutoscaler{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "src-name",
-					Namespace: "src-namespace",
-				},
-				Spec: SpannerAutoscalerSpec{
-					ScaleTargetRef: ScaleTargetRef{
-						ProjectID:  pointer.String("src-project-id"),
-						InstanceID: pointer.String("src-instance-id"),
-					},
-					TargetCPUUtilization: TargetCPUUtilization{
-						HighPriority: pointer.Int32(50),
-					},
-				},
-			},
-			expected: &v1beta1.SpannerAutoscaler{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "src-name",
-					Namespace: "src-namespace",
-				},
-				Spec: v1beta1.SpannerAutoscalerSpec{
-					TargetInstance: v1beta1.TargetInstance{
-						ProjectID:  "src-project-id",
-						InstanceID: "src-instance-id",
-					},
-					ScaleConfig: v1beta1.ScaleConfig{
-						TargetCPUUtilization: v1beta1.TargetCPUUtilization{
-							HighPriority: 50,
-						},
-					},
-				},
-			},
-		},
-		{
-			title: "status",
-			src: &SpannerAutoscaler{
-				Status: SpannerAutoscalerStatus{
-					LastScaleTime:                     &metav1.Time{Time: timestamp},
-					LastSyncTime:                      &metav1.Time{Time: timestamp.Add(-1 * time.Hour)},
-					CurrentNodes:                      pointer.Int32(1),
-					CurrentProcessingUnits:            pointer.Int32(100),
-					DesiredNodes:                      pointer.Int32(2),
-					DesiredProcessingUnits:            pointer.Int32(200),
-					CurrentHighPriorityCPUUtilization: pointer.Int32(10),
-					InstanceState:                     InstanceStateReady,
-				},
-				Spec: SpannerAutoscalerSpec{
-					ScaleTargetRef: ScaleTargetRef{
-						ProjectID:  pointer.String("src-project-id"),
-						InstanceID: pointer.String("src-instance-id"),
-					},
-					TargetCPUUtilization: TargetCPUUtilization{
-						HighPriority: pointer.Int32(50),
-					},
-				},
-			},
-			expected: &v1beta1.SpannerAutoscaler{
-				Status: v1beta1.SpannerAutoscalerStatus{
-					LastScaleTime:                     metav1.Time{Time: timestamp},
-					LastSyncTime:                      metav1.Time{Time: timestamp.Add(-1 * time.Hour)},
-					CurrentNodes:                      1,
-					CurrentProcessingUnits:            100,
-					DesiredNodes:                      2,
-					DesiredProcessingUnits:            200,
-					CurrentHighPriorityCPUUtilization: 10,
-					InstanceState:                     v1beta1.InstanceStateReady,
-				},
-				Spec: v1beta1.SpannerAutoscalerSpec{
-					TargetInstance: v1beta1.TargetInstance{
-						ProjectID:  "src-project-id",
-						InstanceID: "src-instance-id",
-					},
-					ScaleConfig: v1beta1.ScaleConfig{
-						TargetCPUUtilization: v1beta1.TargetCPUUtilization{
-							HighPriority: 50,
-						},
-					},
-				},
-			},
-		},
-	}
+		}
+		timestamp = time.Now()
+	})
 
-	for _, tc := range tests {
-		tc := tc
+	DescribeTable("authentication",
+		func(impersonateConfig *ImpersonateConfig, serviceAccountSecretRef *ServiceAccountSecretRef, expectedAuthentication v1beta1.Authentication) {
+			src.Spec.ImpersonateConfig = impersonateConfig
+			src.Spec.ServiceAccountSecretRef = serviceAccountSecretRef
 
-		t.Run(tc.title, func(t *testing.T) {
-			dest := &v1beta1.SpannerAutoscaler{}
-			err := tc.src.ConvertTo(dest)
-			require.NoError(t, err)
+			expected.Spec.Authentication = expectedAuthentication
 
-			require.Equal(t, tc.expected, dest)
-		})
-	}
-}
+			err := src.ConvertTo(dest)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(dest).To(Equal(expected))
+		},
+		Entry("AuthType is impersonation",
+			&ImpersonateConfig{
+				TargetServiceAccount: "src-target-service-account",
+				Delegates:            []string{"src-delegate1", "src-delegate2"},
+			},
+			nil,
+			v1beta1.Authentication{
+				Type: v1beta1.AuthTypeImpersonation,
+				ImpersonateConfig: &v1beta1.ImpersonateConfig{
+					TargetServiceAccount: "src-target-service-account",
+					Delegates:            []string{"src-delegate1", "src-delegate2"},
+				},
+			},
+		),
+		Entry("AuthType is SA with Namespace",
+			nil,
+			&ServiceAccountSecretRef{
+				Name:      pointer.String("src-service-account-secret-name"),
+				Namespace: pointer.String("src-service-account-secret-namespace"),
+				Key:       pointer.String("src-service-account-secret-key"),
+			},
+			v1beta1.Authentication{
+				Type: v1beta1.AuthTypeSA,
+				IAMKeySecret: &v1beta1.IAMKeySecret{
+					Name:      "src-service-account-secret-name",
+					Namespace: "src-service-account-secret-namespace",
+					Key:       "src-service-account-secret-key",
+				},
+			},
+		),
+		Entry("AuthType is SA without Namespace",
+			nil,
+			&ServiceAccountSecretRef{
+				Name: pointer.String("src-service-account-secret-name"),
+				Key:  pointer.String("src-service-account-secret-key"),
+			},
+			v1beta1.Authentication{
+				Type: v1beta1.AuthTypeSA,
+				IAMKeySecret: &v1beta1.IAMKeySecret{
+					Name: "src-service-account-secret-name",
+					Key:  "src-service-account-secret-key",
+				},
+			},
+		),
+	)
+
+	DescribeTable("scaleConfig",
+		func(minNodes, maxNodes, minPU, maxPU, maxScaleDownNodes *int32, expectedScaleConfig v1beta1.ScaleConfig) {
+			src.Spec.MinNodes = minNodes
+			src.Spec.MaxNodes = maxNodes
+			src.Spec.MinProcessingUnits = minPU
+			src.Spec.MaxProcessingUnits = maxPU
+			src.Spec.MaxScaleDownNodes = maxScaleDownNodes
+
+			expected.Spec.ScaleConfig = expectedScaleConfig
+
+			err := src.ConvertTo(dest)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(dest).To(Equal(expected))
+		},
+		Entry("ComputeType is Node", pointer.Int32(1), pointer.Int32(10), nil, nil, pointer.Int32(1),
+			v1beta1.ScaleConfig{
+				ComputeType: v1beta1.ComputeTypeNode,
+				Nodes: v1beta1.ScaleConfigNodes{
+					Min: 1,
+					Max: 10,
+				},
+				ScaledownStepSize: 1000,
+				TargetCPUUtilization: v1beta1.TargetCPUUtilization{
+					HighPriority: 50,
+				},
+			},
+		),
+		Entry("ComputeType is PU", nil, nil, pointer.Int32(100), pointer.Int32(1000), pointer.Int32(1),
+			v1beta1.ScaleConfig{
+				ComputeType: v1beta1.ComputeTypePU,
+				ProcessingUnits: v1beta1.ScaleConfigPUs{
+					Min: 100,
+					Max: 1000,
+				},
+				ScaledownStepSize: 1000,
+				TargetCPUUtilization: v1beta1.TargetCPUUtilization{
+					HighPriority: 50,
+				},
+			},
+		),
+	)
+
+	DescribeTable("objectMeta",
+		func(objectMeta metav1.ObjectMeta) {
+			src.ObjectMeta = objectMeta
+			expected.ObjectMeta = objectMeta
+
+			err := src.ConvertTo(dest)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(dest).To(Equal(expected))
+		},
+		Entry("Name and Namespace", metav1.ObjectMeta{
+			Name:      "src-name",
+			Namespace: "src-namespace",
+		}),
+	)
+
+	DescribeTable("status",
+		func(status SpannerAutoscalerStatus, expectedStatus v1beta1.SpannerAutoscalerStatus) {
+			src.Status = status
+			expected.Status = expectedStatus
+
+			err := src.ConvertTo(dest)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(dest).To(Equal(expected))
+		},
+		Entry("All statuses", SpannerAutoscalerStatus{
+			LastScaleTime:                     &metav1.Time{Time: timestamp},
+			LastSyncTime:                      &metav1.Time{Time: timestamp.Add(-1 * time.Hour)},
+			CurrentNodes:                      pointer.Int32(1),
+			CurrentProcessingUnits:            pointer.Int32(100),
+			DesiredNodes:                      pointer.Int32(2),
+			DesiredProcessingUnits:            pointer.Int32(200),
+			CurrentHighPriorityCPUUtilization: pointer.Int32(10),
+			InstanceState:                     InstanceStateReady,
+		}, v1beta1.SpannerAutoscalerStatus{
+			LastScaleTime:                     metav1.Time{Time: timestamp},
+			LastSyncTime:                      metav1.Time{Time: timestamp.Add(-1 * time.Hour)},
+			CurrentNodes:                      1,
+			CurrentProcessingUnits:            100,
+			DesiredNodes:                      2,
+			DesiredProcessingUnits:            200,
+			CurrentHighPriorityCPUUtilization: 10,
+			InstanceState:                     v1beta1.InstanceStateReady,
+		}),
+	)
+})
--- go.mod
+++ go.mod
@@ -11,7 +11,6 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/onsi/ginkgo/v2 v2.0.0
 	github.com/onsi/gomega v1.17.0
-	github.com/stretchr/testify v1.7.0
 	go.uber.org/zap v1.19.0
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
@@ -50,7 +49,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.11.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.26.0 // indirect
